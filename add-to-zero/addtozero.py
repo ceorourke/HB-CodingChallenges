136127425053278ac65191c8fcccebcb4f94cd65
vagrant <elena.colleen@gmail.com>
2017-09-15 03:31:03 +0000

    Added multiple solutions

--- add-to-zero/addtozero.py
+++ add-to-zero/addtozero.py
@@ -1,15 +1,15 @@
 """Given list of ints, return True if any two nums in list sum to 0.
 
-    >>> add_to_zero([])
+    >>> add_to_zero_set([])
     False
 
-    >>> add_to_zero([1])
+    >>> add_to_zero_set([1])
     False
 
-    >>> add_to_zero([1, 2, 3])
+    >>> add_to_zero_set([1, 2, 3])
     False
 
-    >>> add_to_zero([1, 2, 3, -2])
+    >>> add_to_zero_set([1, 2, 3, -2])
     True
 
 Given the wording of our problem, a zero in the list will always
@@ -32,10 +32,25 @@ def add_to_zero(nums):
             for n in nums:
                 if num + n == 0:
                     return True
-                    
 
     return False
 
+def add_to_zero_fancy(nums):
+    """Given list of ints, return True if any two nums sum to 0."""
+
+    nums = set(nums)
+
+    return any(-num in nums for num in nums)
+
+def add_to_zero_set(nums):
+    """Given list of ints, return True if any two nums sum to 0."""
+
+    set_nums = set(nums)
+
+    for num in nums:
+        if -num in set_nums:
+            return True
+    return False
 
 if __name__ == '__main__':
     import doctest
