a7943a26f59244af1940537f248baa45e99cf40c
colleen <elena.colleen@gmail.com>
2018-02-11 20:21:35 -0800

    polish notation calculator

---
+++ hackbright_challenges/medium/calculator.py
@@ -0,0 +1,53 @@
+"""Calculator
+
+    >>> calc("+ 1 2")  # 1 + 2
+    3
+
+    >>> calc("* 2 + 1 2")  # 2 * (1 + 2)
+    6
+
+    >>> calc("+ 9 * 2 3")   # 9 + (2 * 3)
+    15
+
+Let's make sure we have non-commutative operators working:
+
+    >>> calc("- 1 2")  # 1 - 2
+    -1
+
+    >>> calc("- 9 * 2 3")  # 9 - (2 * 3)
+    3
+
+    >>> calc("/ 6 - 4 2")  # 6 / (4 - 2)
+    3
+"""
+
+
+def calc(s):
+    """Evaluate expression."""
+
+    result = 0
+    stack = [char for char in s if char != " "]
+    first = int(stack.pop())
+    second = int(stack.pop())
+
+    while stack:
+        operator = stack.pop()
+        if operator == "+":
+            result = second + first
+        elif operator == "*":
+            result = second * first
+        elif operator == "-":
+            result = second - first
+        else:
+            result = second / first
+        if stack:
+            first = result
+            second = int(stack.pop())
+
+    return result
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED; WELL-CALCULATED! ***\n"
